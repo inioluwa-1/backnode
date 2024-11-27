a07eab391da53b8a7e364847fe9f0c39a65e0254
ayanbisi inioluwa <ifeini048@gmail.com>
2024-11-27 09:55:08 +0100

    version three

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const jwt = require("jsonwebtoken")
+const SECRET = process.env.SECRET_KEY
+const bcrypt = require("bcryptjs")
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    let personObject = {
+        email: "him@gmail.com",
+        password: "him123"
+    }
+// bcrypt.genSalt(10, ()=>{
+//     bcrypt.hash(personObject.password, 10, (err, hashP)=>{
+//         return hashP
+        
+//     })
+//     bcrypt.compare(this.password, personObject.password)
+//     .then((result)=>{
+//         console.log(result);
+//         res.send(result)
+        
+//     })
+// })
+
+
+
+jwt.sign(personObject,SECRET, {expiresIn:"1h"}, (error, token)=>{
+    console.log(token);
+    
+} )
 });
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
+
+
+// userSchema.pre("save", function(next){
+//     bcrypt.hash(this.password, 10, (err, hashP)=>{
+//         this.password = hashP
+//     })
+// })
